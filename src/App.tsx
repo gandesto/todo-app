a70d50a9f94450ecd791c49e8f16370f53f93c80
gandesto <ernestojlopezb14@gmail.com>
2024-09-01 08:24:52 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+
+import { useState } from "react"
+import { Todos } from "./components/Todos"
+import { type TodoId, type Todo as TodoType } from "./types"
+
+const mockTodos = [
+  {
+    id : '1',
+    title : 'Learning react  ',
+    completed : true
+  },
+  {
+    id : '2',
+    title : 'using typescripts',
+    completed : false
+  },
+  {
+  id : '3',
+  title : 'and yeah whatever its an 8th',
+  completed : false
+  }
+]
+
+
+const App = (): JSX.Element => {
+  const [todos , setTodos] = useState(mockTodos)
+
+  const handleRemove = ({id}: TodoId):void => {
+    const newTodos = todos.filter(todo => todo.id != id)
+    setTodos(newTodos)
+  }
+
+  const handleCompleted = ( 
+    {id , completed}: Pick<TodoType, 'id'|'completed'>
+  ):void => {
+    const newTodos = todos.map(todo =>{
+      if (todo.id === id){
+        return {
+          ...todo,
+          completed
+        }
+      }
+      return todo
+    })
+
+    setTodos(newTodos)
+  }
+
+
+
+  return (
+    <div className="todoapp">
+      <Todos
+        onToggleCompleteTodo={handleCompleted}
+        onRemoveTodo={handleRemove}
+        todos={todos}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+
+import './index.css'
+import 'todomvc-app-css/index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
